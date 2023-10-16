e8edd78a73752b577dbc433e0c6e91f1847a4d26
Dan Shpaner <dan_shpaner@gmail.com>
2023-10-16 18:14:34 +0300

    feat(numberStringOperations.js): Removed bug for removing digits from a number inexponential basis.

--- js/numberStringOperations.js
+++ js/numberStringOperations.js
@@ -1,3 +1,9 @@
+const canNumberBeModified = (numberString, allowsAdditionalDigit) => {
+    const MAX_DIGITS_ALLOWED_FOR_MODIFICATION = allowsAdditionalDigit ? 13 : 12;
+
+    return numberString.length < MAX_DIGITS_ALLOWED_FOR_MODIFICATION;
+};
+
 const clearMeaninglessSpecialCharacters = (numberString) => {
     let SPECIAL_CHARACTERS = ["-", "."];
     let result = numberString;
@@ -5,22 +11,16 @@ const clearMeaninglessSpecialCharacters = (numberString) => {
     while (SPECIAL_CHARACTERS.some((character) => result.endsWith(character))) {
         result = result.slice(0, -1);
     }
-    
+
     return result;
 }
 
 
 export const removeLastDigit = (numberString) => {
-    return clearMeaninglessSpecialCharacters(numberString.slice(0, -1));
+    return canNumberBeModified(numberString, true) ? clearMeaninglessSpecialCharacters(numberString.slice(0, -1)) : numberString;
 };
 
 
 export const concatenateDigit = (numberString, newDigit) => {
-    const MAX_CONCATINATION_DIGITS = 12;
-
-    if (numberString.length < MAX_CONCATINATION_DIGITS) {
-        return numberString + newDigit;
-    }
-
-    return numberString;
+    return canNumberBeModified(numberString, false) ? numberString + newDigit : numberString;
 };
