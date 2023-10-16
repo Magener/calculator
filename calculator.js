7eaf2fd2c10d7c347891d62d6fc4464a2c0f5bf7
Dan Shpaner <dan_shpaner@gmail.com>
2023-10-16 14:57:22 +0300

    feat(buttons.js, calculator.html, calculator.js, numberStringOperations.js): Refactored files.

--- calculator.js
+++ calculator.js
@@ -1,101 +1,41 @@
 import { shownNumberString, updateShownNumber, valueOfShownNumber, clearShownNumber } from "./shownNumber.js";
+
+import {initializeOperationButtons, initializeDigitButtons} from "./buttons.js";
 import Equation from "./equation.js";
+import { concatenateDigit, removeLastDigit } from "./numberStringOperations.js";
 
 const currentEquation = new Equation();
 
 document.addEventListener("DOMContentLoaded", () => {
     clearShownNumber();
 
-    initializeDigitButtons();
+    initializeDigitButtons((newDigit) => updateShownNumber(concatenateDigit(shownNumberString(), newDigit)));
 
-    initializeOperationButtons();
-});
-
-//TODO: refactor & extract to buttonInitializations
-const initializeOperationButtons = () => {
-
-    initializeButton(document.getElementById("operations"), "=", () => {
-        solveCurrentEquation(valueOfShownNumber());
-    });
-
-    initializeButton(document.getElementById("operations"), "C", () => {
-        removeLastDigit();
-    });
-
-    initializeButton(document.getElementById("operations"), "CE", () => {
-        clearCurrentEquation();
-    });
-
-    const operations = {
+    const computationOperations = {
         "+": (left, right) => left + right,
         "-": (left, right) => left - right,
         "*": (left, right) => left * right,
         "/": (left, right) => left / right,
     };
+    
+    let equationManagementOperations = {
+        "=": () => updateShownNumber(currentEquation.solve(valueOfShownNumber())),
+        "C": () => updateShownNumber(removeLastDigit(shownNumberString())),
+        "CE": clearCurrentEquation
+    
+    };
 
-    Object.entries(operations).forEach(([sign, operationComputation]) => {
-        initializeButton(document.getElementById("operations"), sign, () => performOperation(operationComputation));
-    });
-};
+    initializeOperationButtons(equationManagementOperations, computationOperations, selectOperation);
+});
 
 const clearCurrentEquation = () => {
     clearShownNumber();
     currentEquation.reset();
 };
 
-const solveCurrentEquation = (rightValue) => {
-    updateShownNumber(currentEquation.solve(rightValue));
-};
-
-//TODO: deal with the bug of the -
-// with the same right value
-const performOperation = (operationComputation) => {
+//TODO: deal with the bug of the - with the same right value
+const selectOperation = (operationComputation) => {
     currentEquation.setLeftValue(valueOfShownNumber());
     currentEquation.setOperationComputation(operationComputation);
     clearShownNumber();// TODO: perform after adding an operation
 };
-
-//TODO: extract to stringifiedNumberManagement.js
-const removeLastDigit = () => {
-    let result = shownNumberString().slice(0, -1);
-
-    updateShownNumber(clearMeaninglessSpecialCharacters(result));
-};
-
-//TODO: extract to stringifiedNumberManagement.js
-const concatenateDigit = (value) => {
-    const MAX_CONCATINATION_DIGITS = 12; // TODO: how does that deal with exponential mode? (plaster but works for now)
-
-    if (shownNumberString().length < MAX_CONCATINATION_DIGITS) {
-        updateShownNumber(shownNumberString() + value);
-    }
-};
-
-const initializeDigitButtons = () => {
-    let digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-    digits.forEach((digit) => initializeDigitButton(digit));
-};
-
-
-const initializeDigitButton = (digitValue) => {
-    initializeButton(document.getElementById("digits"), digitValue, () => concatenateDigit(digitValue));
-};
-
-const initializeButton = (parentElement, textContent, clickCallback) => {
-    const button = document.createElement("button");
-    button.textContent = textContent;
-    button.addEventListener("click", () => clickCallback());
-
-    parentElement.appendChild(button);
-}
-
-const clearMeaninglessSpecialCharacters = (numberString) => {
-    let SPECIAL_CHARACTERS = ["-", "."];
-    let result = numberString;
-
-    while (SPECIAL_CHARACTERS.some((character) => result.endsWith(character))) {
-        result = result.slice(0, -1);
-    }
-    return result;
-}
